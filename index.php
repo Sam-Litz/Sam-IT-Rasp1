53da84b4864ebd03625490c5e9af754d9bafa5a0
Sam-Master <samuel.litzler39@gmail.com>
2018-10-17 10:05:09 +0200

    mise à jour index html et php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+if($_POST['Login']=="")
+{
+    echo "<h1>Vous devez saisir un identifiant</h1><br/>";
+}
+    
+    
+if($_POST['Password']=="")
+{
+    echo "<h1>Vous devez saisir un mot de passe</h1><br/>";
+}    
+
+
+
+
+
+?>
